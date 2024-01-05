ef263df45e1942116387274cec78662425d038a6
Frankie Baffa <frankiebaffa@gmail.com>
2024-01-05 11:40:16 -0500

    complete v0.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,300 @@
-struct DateTime;
+//! Provides access to complex datetimes after 1970-01-01T00:00:00 using only
+//! [SystemTime](`std::time::SystemTime`).
 
-const YEAR: usize = 1970;
-const MONTH: usize = 1;
-const DAY: usize = 1;
-const HOUR: usize = 0;
-const MINUTE: usize = 0;
-const SECOND: usize = 0;
+#[cfg(test)]
+mod test;
 
-impl DateTime {
-    pub fn now() {
+const EPOCH_YEAR: usize = 1970;
+const EPOCH_MONTH: usize = 1;
+const EPOCH_DAY: usize = 1;
+
+const DAYS: [&'static str; 7] = [
+    "Thursday", // unix epoch started on a thursday
+    "Friday",
+    "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday"
+];
+
+const MONTHS: [&'static str; 12] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+const MONTH_MAX_DAY: [[usize; 2]; 12] = [
+    [ 31, 31, ],
+    [ 28, 29, ],
+    [ 31, 31, ],
+    [ 30, 30, ],
+    [ 31, 31, ],
+    [ 30, 30, ],
+    [ 31, 31, ],
+    [ 31, 31, ],
+    [ 30, 30, ],
+    [ 31, 31, ],
+    [ 30, 30, ],
+    [ 31, 31, ],
+];
+
+/// A date/time represented in UTC.
+pub struct Utc {
+    year: usize,
+    month: usize,
+    month_of_year: &'static str,
+    day: usize,
+    day_of_week: &'static str,
+    hour: usize,
+    minute:usize,
+    second: usize,
+    nano: f64,
+}
+
+impl Utc {
+    pub fn is_leap_year(year: usize) -> bool {
+        year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+    }
+
+    pub fn leap_year(&self) -> bool {
+        Self::is_leap_year(self.year)
+    }
+
+    pub fn year(&self) -> usize {
+        self.year
+    }
+
+    pub fn month(&self) -> usize {
+        self.month
+    }
+
+    pub fn month_of_year(&self) -> &'static str {
+        self.month_of_year
+    }
+
+    pub fn day(&self)-> usize {
+        self.day
+    }
+
+    pub fn day_of_week(&self) -> &'static str {
+        self.day_of_week
+    }
+
+    pub fn hour(&self) -> usize {
+        self.hour
+    }
+
+    pub fn minute(&self) -> usize {
+        self.minute
+    }
+
+    pub fn second(&self) -> usize {
+        self.second
+    }
+
+    pub fn nano(&self) -> f64 {
+        self.nano
+    }
+
+    pub fn second_with_nano(&self) -> f64 {
+        self.second as f64 + self.nano
+    }
+
+    /// Prints the date/time in yyyy-MM-ddTHH:mm:ss format.
+    pub fn as_rfc3339(&self) -> String {
+        format!(
+            "{:0>4}-{:0>2}-{:0>2}T{:0>2}:{:0>2}:{:0>2}",
+            self.year, self.month, self.day, self.hour, self.minute, self.second
+        )
+    }
+
+    /// Prints the date/time in yyyy-MM-ddTHH:mm:ss.f format with the
+    /// specified precision.
+    pub fn as_rfc3339_with_nano(&self, precision: usize) -> String {
+        let nano = &format!("{:.precision$}", self.nano)[2..];
+        format!(
+            "{:0>4}-{:0>2}-{:0>2}T{:0>2}:{:0>2}:{:0>2}.{nano}",
+            self.year, self.month, self.day, self.hour, self.minute, self.second
+        )
+    }
+
+    fn from_seconds_since_epoch(seconds_with_nanos: f64) -> Self {
+        let total_seconds_since_epoch = seconds_with_nanos as usize;
+        let nano = seconds_with_nanos - total_seconds_since_epoch as f64;
+        let mut minute = total_seconds_since_epoch / 60_usize;
+        let second = total_seconds_since_epoch - (minute * 60_usize);
+        let mut hour = minute / 60_usize;
+        minute -= hour * 60_usize;
+        let days = hour / 24_usize;
+        hour -= days * 24_usize;
+
+        let day_of_week = DAYS[days % DAYS.len()];
+
+        let mut day = EPOCH_DAY;
+        let mut month = EPOCH_MONTH;
+        let mut year = EPOCH_YEAR;
+        for _ in 0..days {
+            let month_days = Self::get_days_in_month(year, month);
+            if day == month_days {
+                day -= month_days - 1;
+                if month == MONTHS.len() {
+                    month -= MONTHS.len() - 1;
+                    year += 1;
+                } else {
+                    month += 1;
+                }
+            } else {
+                day += 1;
+            }
+        }
+
+        let month_of_year = MONTHS[month - 1];
+
+        Self {
+            year,
+            month,
+            month_of_year,
+            day,
+            day_of_week,
+            hour,
+            minute,
+            second,
+            nano,
+        }
+    }
+
+    fn get_days_in_month(year: usize, month: usize) -> usize {
+        MONTH_MAX_DAY[month - 1][if Self::is_leap_year(year) { 1 } else { 0 }]
+    }
+
+    /// Gets a specific date/time.
+    pub fn from_ymdhms(
+        mut year: usize, mut month: usize, mut day: usize,
+        mut hour: usize, mut minute: usize, second_n: f64,
+    ) -> Self {
+        // only allow >= epoch
+        if year < 1970 || year == 1970 && month == 0 {
+            panic!("Date must be >= 1970");
+        }
+
+        // Correct possible zeros
+        if day == 0 && month == 0 {
+            year -= 1;
+            month = 11;
+            day = Self::get_days_in_month(year, month);
+        } else if month == 0 {
+            month = 12;
+            year -= 1;
+        } else if day == 0 {
+            if month == 1 {
+                month = 12;
+                year -= 1;
+            } else {
+                month -= 1;
+            }
+            day = Self::get_days_in_month(year, month);
+        }
+
+        // only allow >= epoch
+        if year < 1970 || year == 1970 && month == 0 {
+            panic!("Date must be >= 1970");
+        }
+
+        let mut second = second_n as usize;
+        let mut nano = second_n - second as f64;
+
+        // Correct overflows from bottom-up.
+        // Overflow nanos into seconds.
+        if nano > 1_f64 {
+            let overflow = (nano - ((nano as usize) as f64)) as usize;
+            second += overflow;
+            nano -= overflow as f64;
+        }
+
+        // Overflow seconds into minutes.
+        if second >= 60 {
+            minute += (second % 60) + 1;
+            second %= 60;
+        }
+
+        // Overflow minutes into hours.
+        if minute >= 60 {
+            hour += (minute % 60) + 1;
+            minute %= 60;
+        }
+
+        // Overflow hours into days.
+        if hour >= 24 {
+            day += (hour % 24) + 1;
+            hour %= 24;
+        }
+
+        // Overflow months into years.
+        if month > 12 {
+            year += month % 12;
+            month %= 12;
+        }
+
+        // Overflow days into months.
+        loop {
+            let mut corrected = false;
+            let days_in_month = Self::get_days_in_month(year, month);
+            if day > days_in_month {
+                corrected = true;
+                day -= days_in_month;
+                month += 1;
+            }
+            if month > 12 {
+                year += 1;
+                month = 1;
+            }
+            if !corrected {
+                break;
+            }
+        }
+
+        // calculate days since epoch
+        let mut days = 0;
+        for y in EPOCH_YEAR..(year + 1) {
+            for m in 1..(MONTHS.len() + 1) {
+                if y != year || m != month {
+                    days += Self::get_days_in_month(y, m);
+                } else {
+                    days += day - 1;
+                    break;
+                }
+            }
+        }
+
+        let hours = (days * 24) + hour;
+        let minutes = (hours * 60) + minute;
+        let seconds = (minutes * 60) + second;
+        let seconds_with_nanos = seconds as f64 + nano;
+        Self::from_seconds_since_epoch(seconds_with_nanos)
+    }
+
+    /// Gets a specific date.
+    pub fn from_ymd(year: usize, month: usize, day: usize) -> Self {
+        Self::from_ymdhms(year, month, day, 0, 0, 0_f64)
+    }
+
+    /// Gets the current date/time.
+    pub fn now() -> Self {
         let since = std::time::SystemTime::now()
             .duration_since(std::time::SystemTime::UNIX_EPOCH)
             .unwrap();
-        let mut seconds = since.as_secs_f64();
-        let mut minutes = seconds / 60_f64;
-        seconds -= minutes;
-        let mut hours = minutes / 60_f64;
+        let total_nanos_since_epoch = since.as_secs_f64();
+        Self::from_seconds_since_epoch(total_nanos_since_epoch)
     }
 }
+
---
+++ src/test.rs
@@ -0,0 +1,132 @@
+#[test]
+fn standard_date() {
+    let dt = crate::Utc::from_ymdhms(2024, 1, 5, 12, 0, 0_f64);
+    assert_eq!("2024-01-05T12:00:00", dt.as_rfc3339());
+}
+
+#[test]
+fn on_leap_day() {
+    let dt = crate::Utc::from_ymdhms(2000, 2, 29, 12, 0, 0_f64);
+    assert_eq!("2000-02-29T12:00:00", dt.as_rfc3339());
+}
+
+#[test]
+fn on_leap_day_non_leap_year() {
+    let dt = crate::Utc::from_ymdhms(2001, 2, 29, 12, 0, 0_f64);
+    assert_eq!("2001-03-01T12:00:00", dt.as_rfc3339());
+}
+
+#[test]
+fn overflow_month() {
+    let dt = crate::Utc::from_ymd(2020, 13, 1);
+    assert_eq!("2021-01-01T00:00:00", dt.as_rfc3339());
+}
+
+#[test]
+fn overflow_day() {
+    let dt = crate::Utc::from_ymd(2020, 12, 32);
+    assert_eq!("2021-01-01T00:00:00", dt.as_rfc3339());
+}
+
+#[test]
+fn overflow_day_and_month() {
+    let dt = crate::Utc::from_ymd(2020, 13, 32);
+    assert_eq!("2021-02-01T00:00:00", dt.as_rfc3339());
+}
+
+#[test]
+fn overflow_hour() {
+    let dt = crate::Utc::from_ymdhms(2018, 11, 11, 24, 0, 0_f64);
+    assert_eq!("2018-11-12T00:00:00", dt.as_rfc3339());
+}
+
+#[test]
+fn overflow_hour_and_day() {
+    let dt = crate::Utc::from_ymdhms(2018, 11, 31, 24, 0, 0_f64);
+    assert_eq!("2018-12-02T00:00:00", dt.as_rfc3339());
+}
+
+#[test]
+fn overflow_hour_day_and_month() {
+    let dt = crate::Utc::from_ymdhms(2018, 13, 32, 24, 0, 0_f64);
+    assert_eq!("2019-02-02T00:00:00", dt.as_rfc3339());
+}
+
+#[test]
+fn overflow_minute() {
+    let dt = crate::Utc::from_ymdhms(2017, 12, 10, 12, 60, 0_f64);
+    assert_eq!("2017-12-10T13:00:00", dt.as_rfc3339());
+}
+
+#[test]
+fn overflow_second() {
+    let dt = crate::Utc::from_ymdhms(2017, 12, 10, 12, 60, 0_f64);
+    assert_eq!("2017-12-10T13:00:00", dt.as_rfc3339());
+}
+
+#[test]
+fn day_of_week() {
+    let dt = crate::Utc::from_ymd(2024, 1, 5);
+    assert_eq!("Friday", dt.day_of_week());
+}
+
+#[test]
+fn day_of_week_of_epoch() {
+    let dt = crate::Utc::from_ymd(1970, 1, 1);
+    assert_eq!("Thursday", dt.day_of_week());
+}
+
+#[test]
+fn day_of_week_day_after_epoch() {
+    let dt = crate::Utc::from_ymd(1970, 1, 2);
+    assert_eq!("Friday", dt.day_of_week());
+}
+
+#[test]
+fn day_of_week_two_days_after_epoch() {
+    let dt = crate::Utc::from_ymd(1970, 1, 3);
+    assert_eq!("Saturday", dt.day_of_week());
+}
+
+#[test]
+fn day_of_week_six_days_after_epoch() {
+    let dt = crate::Utc::from_ymd(1970, 1, 7);
+    assert_eq!("Wednesday", dt.day_of_week());
+}
+
+#[test]
+fn day_of_week_last_day_of_month_of_epoch() {
+    let dt = crate::Utc::from_ymd(1970, 1, 31);
+    assert_eq!("Saturday", dt.day_of_week());
+}
+
+#[test]
+fn day_of_week_one_month_after_epoch() {
+    let dt = crate::Utc::from_ymd(1970, 2, 1);
+    assert_eq!("Sunday", dt.day_of_week());
+}
+
+#[test]
+fn day_of_week_one_year_after_epoch() {
+    let dt = crate::Utc::from_ymd(1971, 1, 1);
+    assert_eq!("Friday", dt.day_of_week());
+}
+
+#[test]
+fn month_of_year() {
+    let dt = crate::Utc::from_ymd(2024, 1, 5);
+    assert_eq!("January", dt.month_of_year());
+}
+
+#[test]
+fn month_of_year_one_month_after_epoch() {
+    let dt = crate::Utc::from_ymd(2024, 2, 1);
+    assert_eq!("February", dt.month_of_year());
+}
+
+#[test]
+fn with_nanos_2_precision() {
+    let dt = crate::Utc::from_ymdhms(2024, 1, 5, 5, 34, 12.324_f64);
+    assert_eq!("2024-01-05T05:34:12.32", dt.as_rfc3339_with_nano(2));
+}
+
